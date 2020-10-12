2f0594c31f61615865e2824879b37f5143d6ff37
Mulitate4 <aryan.dongre1605@gmail.com>
2020-10-12 14:00:18 +0530

    added collisionmasking

--- phys_game.js
+++ phys_game.js
@@ -1,4 +1,3 @@
-
 let engine = Matter.Engine.create();
 
 const width = 1280, height = 720;
@@ -20,28 +19,65 @@ let mouseConstraint = Matter.MouseConstraint.create(engine, {
     constraint: {
         render: {visible: false},
     },
+    collisionFilter: {
+        'group': 2,
+        'category': 2,
+        'mask': 0,
+    }
 });
 
-let platform = Matter.Bodies.rectangle(700, 300, 300, 20, { isStatic: true });
-let stack = Matter.Composites.stack(550, 200, 5, 5, 0, 0, (x, y) => Matter.Bodies.polygon(x, y, 8, 30));
+let platform = Matter.Bodies.rectangle(700, 300, 300, 20, { 
+    isStatic: true,
+    collisionFilter: {
+        'group': 1,
+        'category': 2,
+        'mask': 0,
+    }
+});
+let stack = Matter.Composites.stack(550, 200, 5, 5, 0, 0, (x, y) => {
+    return Matter.Bodies.polygon(x, y, 8, 30, {
+        collisionFilter: {
+            'group': 1,
+            'category': 2,
+            'mask': 0,
+        },
+    });
+});
 
-let ground = Matter.Bodies.rectangle(400,600,810,60,{ isStatic: true});
+let ground = Matter.Bodies.rectangle(400,600,810,60,{ 
+    isStatic: true,
+    collisionFilter: {
+        'group': 1,
+        'category': 2,
+        'mask': 0,
+    },
+});
 
-let ball = Matter.Bodies.circle(200, 200, 20);
+let ball_create = () => Matter.Bodies.circle(200, 200, 20, {
+    collisionFilter: {
+        'group': 2,
+        'category': 2,
+        'mask': 0,
+}});
+
+ball = ball_create()
 let constraint = Matter.Constraint.create({
     pointA: {x: 200, y: 200},
     bodyB: ball,
-    stiffness: 0.05    
+    stiffness: 0.05,
+    render: {visible: false} 
 })
 
 let firing = false;
 Matter.Events.on(mouseConstraint, 'enddrag', (e) => {
-    if (e.body == ball) firing = true;
+    if (e.body == ball) {
+        firing = true; 
+        ball.collisionFilter.group = 1};
 })
 
 Matter.Events.on(engine, 'afterUpdate', () => {
     if (firing && Math.abs(ball.position.x-200) < 20 && Math.abs(ball.position.y-200) < 20){
-        ball = Matter.Bodies.circle(200, 200, 20);
+        ball = ball_create()
         Matter.World.add(engine.world, ball);
         constraint.bodyB = ball;
         firing = false
